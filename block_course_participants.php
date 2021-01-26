046e6195f0765731b702b7b9a6a3f89e820296d0
flocko-motion <florian.metzger@gmail.com>
2021-01-26 11:52:01 +0100

    release

---
+++ block_course_participants.php
@@ -0,0 +1,120 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Class definition for the course participants block.
+ *
+ * @package    block_course_participants
+ * @copyright  2019 Leo Auri <code@leoauri.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Course participants block class.
+ *
+ * @package    block_course_participants
+ * @copyright  2019 Leo Auri <code@leoauri.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_course_participants extends block_list {
+    /**
+     * Array of user objects of course participants
+     */
+    protected $_participants = null;
+
+    protected function magic_get_participants() {
+        if ($this->_participants === null) {
+            // global $DB;
+            $context = context_course::instance($this->page->course->id);
+            $this->_participants = get_role_users(5, $context);
+        }
+        return $this->_participants;
+    }
+
+    public function __get($name) {
+        $getmethod = 'magic_get_' . $name;
+        if (method_exists($this, $getmethod)) {
+            return $this->$getmethod();
+        } else {
+            return parent::__get();
+        }
+    }
+
+    public function init() {
+        $this->title = get_string('pluginname', 'block_course_participants');
+    }
+
+    public function applicable_formats() {
+        return array('course-view' => true);
+    }
+
+    /**
+     * Returns the contents.
+     *
+     * @return stdClass contents of block
+     */
+    public function get_content() {
+        if (isset($this->content)) {
+            return $this->content;
+        }
+
+        global $DB;
+        $presencemoduleid = $DB->get_field('modules', 'id', ['name' => 'presence']);
+        if ($presencemoduleid) {
+            $result = $DB->get_records('course_modules', [
+                'module' => $presencemoduleid,
+                'course' => $this->page->course->id
+            ]);
+            if (count($result)) {
+                $result = array_pop($result);
+                $presencecmid = $result->id;
+            } else {
+                $presencecmid = null;
+            }
+        } else {
+            $presencecmid = null;
+        }
+
+        $viewpresenceprofile = has_any_capability( ['mod/presence:viewreports'], $this->context);
+
+        $this->content = new stdClass();
+        $this->content->items = array();
+        foreach ($this->participants as $participant) {
+            if ($viewpresenceprofile) {
+                if ($presencecmid) {
+                    $url = new moodle_url(
+                        '/mod/presence/userprofile.php',
+                        ['id' => $presencecmid, 'userid' => $participant->id]
+                    );
+                } else {
+                    $url = new moodle_url(
+                        '/user/view.php',
+                        ['id' => $participant->id, 'course' => $this->page->course->id]
+                    );
+                }
+                $this->content->items[] = html_writer::link(
+                    $url,
+                    fullname($participant)
+                );
+            } else {
+                $this->content->items[] = fullname($participant);
+            }
+        }
+
+        $this->content->icons = array();
+        return $this->content;
+    }
+}
